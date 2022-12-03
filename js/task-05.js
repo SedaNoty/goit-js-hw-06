12b9815800835d1640edc282e6b18154a5f53290
SedaNoty <sedanoty5@gmail.com>
2022-12-04 01:28:46 +0200

    hw-06_task-5

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
+
+nameInput.addEventListener('input', event => {
+	nameOutput.textContent = event.target.value
+
+	if (event.target.value === '') {
+		nameOutput.textContent = 'Anonymous'
+	}
+});
