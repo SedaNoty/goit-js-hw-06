547cf96e549ddadfb15de313ccb88d426379f212
SedaNoty <sedanoty5@gmail.com>
2022-12-04 12:50:58 +0200

    hw-06_task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
+
+
+const pressTheButton = document.querySelector('button.change-color')
+const body = document.querySelector('body')
+const colorBody = document.querySelector('span.color')
+
+
+pressTheButton.addEventListener('click', e => {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorBody.textContent = color;
+});
