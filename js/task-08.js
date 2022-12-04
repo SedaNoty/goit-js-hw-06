c2ca9bd12ad5e0a044f7ef82b21103544ff8db0c
SedaNoty <sedanoty5@gmail.com>
2022-12-04 11:53:24 +0200

    hw-06_task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
 
+
+const formLogin = document.querySelector('.login-form')
+
+formLogin.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+    event.preventDefault()
+    const {
+        elements: { email, password },
+    } = event.currentTarget
+
+    if (email.value === '' || password.value === '') {
+        return alert('Заполните все поля!')
+    }
+    const userData = { email: email.value, Password: password.value }
+    
+    console.log(userData)
+    event.currentTarget.reset()
+};
+
